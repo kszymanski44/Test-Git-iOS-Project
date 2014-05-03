0001ac160365a3467ab50170420ff0a377689daa
Kyle Szymanski <kyle@Kyles-MacBook-Pro-4.local>
2014-05-03 17:51:52 -0500

    added a test class

---
+++ KSTest.h
@@ -0,0 +1,13 @@
+//
+//  KSTest.h
+//  Git Test
+//
+//  Created by Kyle Szymanski on 5/3/14.
+//  Copyright (c) 2014 Ruler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSTest : NSObject
+
+@end
---
+++ KSTest.m
@@ -0,0 +1,13 @@
+//
+//  KSTest.m
+//  Git Test
+//
+//  Created by Kyle Szymanski on 5/3/14.
+//  Copyright (c) 2014 Ruler. All rights reserved.
+//
+
+#import "KSTest.h"
+
+@implementation KSTest
+
+@end
