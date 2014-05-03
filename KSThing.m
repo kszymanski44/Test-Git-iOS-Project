d531a702bd3dd87e274d5ca1673bdc185da7c314
Kyle Szymanski <kyle@Kyles-MacBook-Pro-4.local>
2014-05-03 18:02:02 -0500

    Some changes

---
+++ KSThing.m
@@ -0,0 +1,13 @@
+//
+//  KSThing.m
+//  Git Test
+//
+//  Created by Kyle Szymanski on 5/3/14.
+//  Copyright (c) 2014 Ruler. All rights reserved.
+//
+
+#import "KSThing.h"
+
+@implementation KSThing
+
+@end
